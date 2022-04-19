3b4db653a9fbb976d9922ec978cd95774e119191
Kaylee <06.kaylee@gmail.com>
2022-04-18 23:04:44 -0600

    test comment

--- backend/conn.js
+++ backend/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-// setting up mongo connection
+// setting up mongo connection to db
 const url = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster1.pzfrv.mongodb.net/mealPlannerDb?retryWrites=true&w=majority`;
 const conn = mongoose.connect(url)
   .then(() => {
